faf031fe63a3b715383fd77b6806bf595ce76e88
realtnt <theo@thetaphoto.co.uk>
2022-07-04 14:41:35 +0100

    first commit

---
+++ src/main/java/com/motivatr/dvamt/DvamtApplication.java
@@ -0,0 +1,13 @@
+package com.motivatr.dvamt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvamtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvamtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motivatr/dvamt/DvamtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivatr.dvamt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvamtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
