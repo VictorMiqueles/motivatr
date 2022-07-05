e80602fe2d1e3fcb11c06bb586d753d39da5b565
realtnt <theo@thetaphoto.co.uk>
2022-07-05 10:23:04 +0100

    copied acebook login and stuff

--- src/main/java/com/davmt/motivatr/MotivatrApplication.java
+++ src/main/java/com/davmt/motivatr/MotivatrApplication.java
@@ -1,13 +1,15 @@
 package com.davmt.motivatr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MotivatrApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(MotivatrApplication.class, args);
   }
-
 }
---
+++ src/main/java/com/davmt/motivatr/model/User.java
@@ -0,0 +1,47 @@
+package com.davmt.motivatr.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import lombok.Data;
+
+import javax.persistence.GenerationType;
+
+import static java.lang.Boolean.TRUE;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String username;
+  private String email;
+  private String password;
+  private String firstName;
+  private String lastName;
+  private String imageUrl;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+  private Boolean enabled;
+  @Transient
+  private String passwordConfirm;
+
+  public User() {
+
+  }
+
+  public User(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.createdAt = LocalDateTime.now();
+    this.enabled = TRUE;
+  }
+}
