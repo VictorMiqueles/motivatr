561a4c1902a25c6065fba0cf0b259eb5b5fca064
Matt Clements <matthewlclements@gmail.com>
2022-07-07 13:47:48 +0100

    Started profile test

---
+++ src/test/java/com/davmt/motivatr/integration/ProfileTest.java
@@ -0,0 +1,19 @@
+package com.davmt.motivatr.integration;
+
+import com.github.javafaker.Faker;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ProfileTest {
+  
+}
