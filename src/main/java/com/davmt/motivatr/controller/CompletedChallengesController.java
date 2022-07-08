871e5df6f680b79d927caa33a15be3d9b21abc41
Dominic Brown <dominicbrown@Dominics-MacBook-Air.local>
2022-07-08 16:09:04 +0100

    check if chalenge has been done

--- src/main/java/com/davmt/motivatr/controller/CompletedChallengesController.java
+++ src/main/java/com/davmt/motivatr/controller/CompletedChallengesController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -35,10 +36,9 @@ public class CompletedChallengesController {
     UserService userService;
 
     @PostMapping("/completedChallenges")
-    public RedirectView create(Principal principal) {
+    public RedirectView markAsDone(Principal principal) {
         
         User user = userService.getUserFromPrincipal(principal);
-        Long userId = user.getId();
         Challenge challenge = challengeService.getTodaysChallenge();
 
         CompletedChallenge completedChallenge = new CompletedChallenge();
@@ -49,6 +49,41 @@ public class CompletedChallengesController {
         completedChallengeRepository.save(completedChallenge);
         
         return new RedirectView("/home");
+    }
+
+    @DeleteMapping("/notCompletedChallenges")
+    public RedirectView markAsNotDone(Principal principal) {
+        User user = userService.getUserFromPrincipal(principal);
+        Challenge challenge = challengeService.getTodaysChallenge();
+
+        CompletedChallenge completedChallenge = new CompletedChallenge();
+
+        completedChallenge.setUser(user);
+        completedChallenge.setChallenge(challenge);
+
+        completedChallengeRepository.delete(completedChallenge);
+        
+        return new RedirectView("/home");
+    }
+
+
+
+    @GetMapping("/completedChallenges/check")
+    public String checkIfdone(Model model, Principal principal) {
+        Challenge todaysChallenge = challengeService.getTodaysChallenge();
+        User user = userService.getUserFromPrincipal(principal);
+
+        Long todaysChallengeId = todaysChallenge.getId();
+        Long userId = user.getId();
+
+        List<CompletedChallenge> usersCompletedChallenges = completedChallengeRepository.findByUserId(userId);
+
+        String completedChallengeToday = "FALSE"; 
+
+        if (usersCompletedChallenges.contains(todaysChallengeId)) {
+                String completedChallengeToday = "TRUE";
+        }
+        model.addAttribute("todaysChallengeCheck", completedChallengeToday);
     }
 
 }
--- src/main/java/com/davmt/motivatr/model/CompletedChallenge.java
+++ src/main/java/com/davmt/motivatr/model/CompletedChallenge.java
@@ -43,4 +43,5 @@ public class CompletedChallenge {
     this.completedAt = LocalDateTime.now();
   }
 
+
 }
