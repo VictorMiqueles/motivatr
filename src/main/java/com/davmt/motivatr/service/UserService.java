60aeb5d3d0d312e22c7dad25c804823cdd29f093
realtnt <theo@thetaphoto.co.uk>
2022-07-14 15:25:47 +0100

    removing unused import

--- src/main/java/com/davmt/motivatr/service/UserService.java
+++ src/main/java/com/davmt/motivatr/service/UserService.java
@@ -1,7 +1,6 @@
 package com.davmt.motivatr.service;
 
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
